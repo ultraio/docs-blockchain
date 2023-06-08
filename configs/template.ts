3be6fa648f2f0daf1e4d4d2de39b818a95cf6952
trevor@ultra.io <trevor@ultra.io>
2023-06-08 12:30:19 -0600

    SEO Header Helpers

--- configs/template.ts
+++ configs/template.ts
@@ -4,6 +4,9 @@ import { fileURLToPath } from 'node:url';
 
 const BASE_URL = '/';
 
+const description =
+    'Documentation for the Ultra.io Blockchain and various other APIs. Learn about our blockchain, uniqs, technology, and much more.';
+
 export const templateConfig: UserConfig<DefaultTheme.Config> = {
     base: BASE_URL,
     title: 'Ultra.io Blockchain Docs',
@@ -36,6 +39,19 @@ export const templateConfig: UserConfig<DefaultTheme.Config> = {
         ['link', { rel: 'manifest', href: BASE_URL + 'site.webmanifest' }],
         ['link', { rel: 'mask-icon', href: BASE_URL + 'safari-pinned-tab.svg', color: '#5bbad5' }],
         ['meta', { name: 'msapplication-TileColor', content: '#da532c' }],
+        // Open Graph
+        ['meta', { name: 'description', content: description }],
+        ['meta', { property: 'og:type', content: 'website' }],
+        ['meta', { property: 'og:url', content: 'https://docs.ultra.io' }],
+        ['meta', { property: 'og:title', content: 'Ultra.io Documentation' }],
+        ['meta', { property: 'og:description', content: description }],
+        ['meta', { property: 'og:image', content: '/meta.png' }],
+        // Twitter
+        ['meta', { property: 'twitter:card', content: 'summary_large_image' }],
+        ['meta', { property: 'twitter:url', content: 'https://docs.ultra.io' }],
+        ['meta', { property: 'twitter:title', content: 'Ultra.io Docs' }],
+        ['meta', { property: 'twitter:description', content: description }],
+        ['meta', { property: 'twitter:image', content: '/meta.png' }],
     ],
     vue: {
         template: {

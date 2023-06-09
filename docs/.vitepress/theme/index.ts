b8a1ecee15ef78833d2ae3f573f1ddb33587e602
trevor@ultra.io <trevor@ultra.io>
2023-06-09 08:43:00 -0600

    Enable Giscus

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -17,24 +17,21 @@ export default {
     setup() {
         const { frontmatter } = useData();
         const route = useRoute();
-
-        // ! - TODO: THIS NEEDS TO BE FILLED OUT AFTER GOING PUBLIC
-        // !- https://giscus.app/
-        // giscusTalk(
-        //     {
-        //         repo: 'ultraio/docs-blockchain',
-        //         repoId: 'MDEwOlJlcG9zaXRvcnkyOTg3MTM0NTg=',
-        //         category: 'Documentation Feedback',
-        //         categoryId: 'MDE4OkRpc2N1c3Npb25DYXRlZ29yeTMyMjY2NDY0',
-        //         mapping: 'pathname',
-        //         term: 'Leave a comment!',
-        //         reactionsEnabled: '1',
-        //         lang: 'en',
-        //     },
-        //     {
-        //         frontmatter,
-        //         route,
-        //     }
-        // );
+        giscusTalk(
+            {
+                repo: 'ultraio/docs-blockchain',
+                repoId: 'R_kgDOJZVdMQ',
+                category: 'Documentation Feedback',
+                categoryId: 'DIC_kwDOJZVdMc4CV7Lb',
+                mapping: 'pathname',
+                term: 'Leave a comment!',
+                reactionsEnabled: '1',
+                lang: 'en',
+            },
+            {
+                frontmatter,
+                route,
+            }
+        );
     },
 };

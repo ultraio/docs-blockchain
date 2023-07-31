11b2e33382c19d5c8526d0b7f0af06332f529236
Trevor-Ultra <Trevor-Ultra@users.noreply.github.com>
2023-07-31 14:54:39 +0000

    deploy: a0a6dd6cb7d1123156e953b208c219705dd61824

---
+++ staging/assets/chunks/Experimental.56d94644.js
@@ -0,0 +1,1 @@
+import{u as t,d as s,A as n,N as o,z as r,B as u}from"./framework.ead9a57c.js";const p=t({__name:"Experimental",setup(l){let e=s(!1);return n(()=>{window&&(e.value=window.location.pathname.includes("/experimental"))}),(a,i)=>o(e)?r(a.$slots,"default",{key:0}):u("",!0)}});export{p as default};

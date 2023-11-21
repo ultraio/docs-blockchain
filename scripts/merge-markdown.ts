7e2d0d81a85f7f1147332c41250f79a64fc423b6
trevor-ultra <trevor@ultra.io>
2023-11-21 14:09:31 -0700

    Fix All Docs Download

--- scripts/merge-markdown.ts
+++ scripts/merge-markdown.ts
@@ -7,9 +7,9 @@ files.sort();
 for (let file of files) {
     const content = fs.readFileSync(file);
 
-    if (!fs.existsSync('dist')) {
-        fs.mkdirSync('dist');
+    if (!fs.existsSync('dist/downloads')) {
+        fs.mkdirSync('dist/downloads', { recursive: true });
     }
 
-    fs.appendFileSync('dist/everything.md', '\r\n' + content);
+    fs.appendFileSync('dist/downloads/docs.md', '\r\n' + content);
 }
